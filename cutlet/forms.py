6cc269ee0513f4b5fa28484951b85315a4b0a6ec
justparthi <justparthiban@gmail.com>
2024-09-19 21:53:57 +0530

    db updated

--- cutlet/forms.py
+++ cutlet/forms.py
@@ -4,4 +4,4 @@ from .models import Place
 class PlaceForm(forms.ModelForm):
     class Meta:
         model = Place
-        fields = ['name', 'map_url', 'img_url', 'location', 'has_sockets', 'has_toilet', 'has_wifi', 'can_take_calls', 'seats', 'coffee_price']
+        fields = ['name', 'map_url', 'img_url', 'location', 'has_sockets', 'has_toilet', 'has_wifi', 'can_take_calls', 'seats', 'coffee_price', 'stable_wifi', 'power_sockets', 'quiet', 'calls', 'tables', 'coffee', 'food', 'veggie', 'alcohol', 'payment', 'light', 'outdoor', 'spacious', 'restroom', 'air', 'smoke_free', 'pet', 'parking', 'rating']
